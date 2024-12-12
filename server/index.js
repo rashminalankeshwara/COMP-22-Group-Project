01863b114ab68e86c46745f0657fab803c1f0597
rashminalankeshwara <rmohotti@my.centennialcollege.ca>
2024-12-12 11:03:08 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import {users,posts} from "./data/index.js";
 
+
 /* CONGRATULATION */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -54,6 +55,7 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
+
 
 mongoose
     .connect(process.env.MONGO_URL, {
